344e371b87942b79aa2dbf2d6210fe5a0bf7c562
Lizzie Koehler and Tyler Brown <elementalplug@gmail.com>
2015-02-19 10:55:03 -0800

    add zip functionality

--- js/scripts.js
+++ js/scripts.js
@@ -5,37 +5,36 @@ $(document).ready(function() {
     var inputtedFirstName = $("#new-first-name").val();
     var inputtedLastName  = $("#new-last-name").val();
 
-    var inputtedStreet    = $("#new-street").val();
-    var inputtedCity      = $("#new-city").val();
-    var inputtedState     = $("#new-state").val();
-    var inputtedZip       = $("#new-zip").val();
-
-    var newAddress = { street: inputtedStreet,
-                       city: inputtedCity,
-                       state: inputtedState,
-                       zip: inputtedZip,
-                        fullAddress: function() {
-                         return this.street +
-                         ", " +
-                         this.city +
-                         ", " +
-                         this.state +
-                         " " +
-                         this.zip;
-                       }
-                     };
 
     var newContact = { firstName: inputtedFirstName,
                        lastName: inputtedLastName,
-                       address: newAddress.fullAddress()
-                      };
+                       addresses: []
+                     };
+
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var inputtedZip = $(this).find("input.new-zip").val();
+
+      var newAddress = {
+        street: inputtedStreet,
+        city: inputtedCity,
+        state: inputtedState,
+        zip: inputtedZip
+      }
+
+      newContact.addresses.push(newAddress);
+    });
+
+
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>")
 
 
     $("#new-first-name").val("");
     $("#new-last-name").val("");
-    $("#new-street").val("");
+    $(".new-street").val("");
     $(".new-city").val("");
     $(".new-state").val("");
     $(".new-zip").val("");
@@ -45,27 +44,33 @@ $(document).ready(function() {
       $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
-      $(".address").text(newContact.address);
+      // $(".address").text(newContact.address);
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + ", " + address.zip + "</li>");
+      });
     });
   });
 
+
   $("#add-address").click(function() {
-    $("#new-addresses").append('<div class="new-address">' +
+    $("#new-addresses").append('' +
+    '<div class="new-address">' +
+      '<div class="form-group">' +
+        '<label for="new-street">Street</label>' +
+        '<input type="text" class="form-control new-street">' +
+      '</div>' +
+      '<div class="form-group">' +
+        '<label for="new-city">City</label>' +
+        '<input type="text" class="form-control new-city">' +
+      '</div>' +
+      '<div class="form-group">' +
+        '<label for="new-state">State</label>' +
+        '<input type="text" class="form-control new-state">' +
+      '</div>' +
     '<div class="form-group">' +
-    '<label for="new-street">Street</label>' +
-    '<input type="text" class="form-control new-street">' +
-    '</div>' +
-    '<div class="form-group">' +
-    '<label for="new-city">City</label>' +
-    '<input type="text" class="form-control new-city">' +
-    '</div>' +
-    '<div class="form-group">' +
-    '<label for="new-state">State</label>' +
-    '<input type="text" class="form-control new-state">' +
-    '</div>' +
-    '</div>' + '<div class="new-zip">' + '<div class="form-group">' +
-    '<label for="new-zip">Zip</label>' +
-    '<input type="text" class="form-control new-zip">' +
+      '<label for="new-zip">Zip</label>' +
+      '<input type="text" class="form-control new-zip">' +
     '</div>' + '</div>');
   });
 });
